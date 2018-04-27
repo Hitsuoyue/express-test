4486f51699d5014b390425cb28f9c4e19f60f01c
suoyue <1255915270@qq.com>
2018-04-27 18:18:56 +0800

    'insert'

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +26,8 @@ let corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -45,15 +49,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-//设置跨域访问
-// app.all('*', function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-//     res.header("X-Powered-By",' 3.2.1')
-//     res.header("Content-Type", "application/json;charset=utf-8");
-//     next();
-// });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+let app = express();
 let mysql      = require('mysql');
 
 let connection = mysql.createConnection({
@@ -7,16 +8,44 @@ let connection = mysql.createConnection({
     port:3306,
     user:'root',
     password:'123456789sy',
-    database:'sys'
+    database:'news'
 });
 // connection.connect();
 
 /* GET home page. */
+// app.get('/', function (req, res) {
+//     console.log(req.body);
+//     res.json(req.body);
+// })
 router.get('/', function(req, res, next) {
-    connection.query('SELECT * FROM `sys_config`', function (error, results, fields) {
+    let sql = 'SELECT * FROM news';
+    connection.query(sql, function (error, results, fields) {
         if (error) {
-            console.log(error);
-            throw error;
+            res.json(error);
+        }
+        res.json(results);
+    });
+});
+router.post('/insert', function (req, res, next) {
+
+    if (req.body) {
+        //能正确解析 json 格式的post参数
+        console.log('req.body', req.body);
+    }
+    let id = 4;
+    // let sql = `INSERT INTO news(id,type,title,image,time,src) SELECT 4, '本地', '本地消息', './images/2.jpg', '2012/02/12', 'changAn' FROM DUAL WHERE NOT EXISTS (SELECT * FROM news WHERE id=${id})`; //判断表内不存在 插入数据
+
+    let sql = `INSERT INTO news(id,type,title,image,time,src) SELECT ${req.body.id || 1}, '${req.body.type || ''}', '${req.body.title || ''}', '${req.body.image || ''}', '${req.body.time || ''}', '${req.body.src || ''}' FROM DUAL WHERE NOT EXISTS (SELECT * FROM news WHERE id=${req.body.id || 1})`; //判断表内不存在 插入数据
+    console.log('sql', sql);
+
+    // let sql = `SELECT * FROM news WHERE id=1`; //选择表 user
+    // let sql = ` WHERE NOT EXISTS(SELECT id FROM news WHERE id=1)`;
+    // let sql = `INSERT INTO news(id,type,title,image,time,src) VALUES ('2', '本地', '本地消息', './images/2.jpg', '2012/02/12', 'changAn')`; //判断表内不存在 插入数据
+
+    // let sql = 'SELECT * FROM news';
+    connection.query(sql, function (error, results, fields) {
+        if (error) {
+            res.json(error);
         }
         res.json(results);
     });
