3f010edf07f48c004049c730d4c3c5631f946eec
suoyue <1255915270@qq.com>
2018-04-28 18:04:35 +0800

    'edit'

--- routes/index.js
+++ routes/index.js
@@ -26,8 +26,9 @@ router.get('/', function(req, res, next) {
         res.json(results);
     });
 });
-router.post('/insert', function (req, res, next) {
 
+//插入新闻
+router.post('/insert', function (req, res, next) {
     if (req.body) {
         //能正确解析 json 格式的post参数
         console.log('req.body', req.body);
@@ -37,6 +38,55 @@ router.post('/insert', function (req, res, next) {
 
     let sql = `INSERT INTO news(id,type,title,image,time,src) SELECT ${req.body.id || 1}, '${req.body.type || ''}', '${req.body.title || ''}', '${req.body.image || ''}', '${req.body.time || ''}', '${req.body.src || ''}' FROM DUAL WHERE NOT EXISTS (SELECT * FROM news WHERE id=${req.body.id || 1})`; //判断表内不存在 插入数据
     console.log('sql', sql);
+
+    // let sql = `SELECT * FROM news WHERE id=1`; //选择表 user
+    // let sql = ` WHERE NOT EXISTS(SELECT id FROM news WHERE id=1)`;
+    // let sql = `INSERT INTO news(id,type,title,image,time,src) VALUES ('2', '本地', '本地消息', './images/2.jpg', '2012/02/12', 'changAn')`; //判断表内不存在 插入数据
+
+    // let sql = 'SELECT * FROM news';
+    connection.query(sql, function (error, results, fields) {
+        if (error) {
+            res.json(error);
+        }
+        res.json(results);
+    });
+});
+
+//删除新闻
+router.post('/delete', function (req, res, next) {
+    if (req.body) {
+        //能正确解析 json 格式的post参数
+        console.log('req.body', req.body);
+    }
+
+
+    let sql = `DELETE FROM news WHERE id=${req.body.id}`;
+    console.log('sql', sql);
+
+
+    // let sql = `SELECT * FROM news WHERE id=1`; //选择表 user
+    // let sql = ` WHERE NOT EXISTS(SELECT id FROM news WHERE id=1)`;
+    // let sql = `INSERT INTO news(id,type,title,image,time,src) VALUES ('2', '本地', '本地消息', './images/2.jpg', '2012/02/12', 'changAn')`; //判断表内不存在 插入数据
+
+    // let sql = 'SELECT * FROM news';
+    connection.query(sql, function (error, results, fields) {
+        if (error) {
+            res.json(error);
+        }
+        res.json(results);
+    });
+});
+
+//修改新闻
+router.post('/edit', function (req, res, next) {
+    if (req.body) {
+        //能正确解析 json 格式的post参数
+        console.log('req.body', req.body);
+    }
+
+    let sql = `UPDATE news SET type='${req.body.type || ''}', title='${req.body.title || ''}', image='${req.body.image || ''}', time='${req.body.time || ''}', src='${req.body.src || ''}' WHERE id=${req.body.id}`;
+    console.log('sql', sql);
+
 
     // let sql = `SELECT * FROM news WHERE id=1`; //选择表 user
     // let sql = ` WHERE NOT EXISTS(SELECT id FROM news WHERE id=1)`;
